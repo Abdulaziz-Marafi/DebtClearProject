18c11dc6c31772457131581b019c495e82dd2b4d
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2025-02-02 12:37:30 +0300

    Added Account

---
+++ DebtClearProject/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using DebtClearProject.Models;
+using DebtClearProject.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebtClearProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region InjectedServices
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+        private RoleManager<IdentityRole> roleManager;
+        public AccountController(UserManager<User> _userManager, SignInManager<User> _signInManager, RoleManager<IdentityRole> _roleManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+            roleManager = _roleManager;
+        }
+        #endregion
+
+        #region Users
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the user to the database
+                User user = new User()
+                {
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+
+                }
+            }
+            return View(model);
+        }
+        [AllowAnonymous]
+        public IActionResult Login() { return View(); }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid Email or Password.");
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Logout(int? id)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+    }
+}
