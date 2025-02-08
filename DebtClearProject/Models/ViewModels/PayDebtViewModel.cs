5d7105d0ad4b22bcb9bf4459bbc5da948256225b
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2025-02-08 14:02:51 +0300

    fixed debt payments

--- DebtClearProject/Models/ViewModels/PayDebtViewModel.cs
+++ DebtClearProject/Models/ViewModels/PayDebtViewModel.cs
@@ -6,7 +6,7 @@ namespace DebtClearProject.Models.ViewModels
     {
         public Guid UserDebtId { get; set; }
 
-        [Range(0.01, 10000, ErrorMessage ="Amount Must be positive and below 10000.")]
+        [Range(0.01, 10000, ErrorMessage ="Amount Must be positive and between (0.01 - 10000).")]
         public decimal Amount { get; set; }
 
         public decimal RemainingDebt { get; set; }
